c502ed889de7de6e90c215013893f0070c348468
abdoessordo <abdoessordo01@gmail.com>
2022-10-18 15:08:20 +0100

    fixed miner bug

--- client/src/api_routes.js
+++ client/src/api_routes.js
@@ -1,4 +1,4 @@
-const baseURL = "https://reddragon-13cz.onrender.com/api/";
+export const baseURL = "https://reddragon-13cz.onrender.com/api/";
 // export const baseURL = "http://127.0.0.1:8000/api/";
 
 export const getAllReviewsRoute = `${baseURL}blogs/all`;
--- server/config/database.js
+++ server/config/database.js
@@ -2,16 +2,6 @@ const { Sequelize } = require("sequelize");
 // const setAssocations = require("../associations/setAssocations");
 require("dotenv").config();
 
-// const Connection = {
-//   dialect: "postgres",
-//   host: "localhost",
-//   port: "5432",
-//   username: process.env.DB_USERNAME,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   logging: false,
-// };
-
 const Connection = {
   dialect: "postgres",
   host: process.env.DB_HOST,
@@ -29,7 +19,7 @@ const Connection = {
 
 
 const sequelize = new Sequelize(Connection);
-sequelize.sync({ alter: true });
+sequelize.sync({ alter: false });
 console.log("All models were synchronized successfully.");
 sequelize.query("CREATE EXTENSION IF NOT EXISTS pg_trgm", { raw: true });
 
