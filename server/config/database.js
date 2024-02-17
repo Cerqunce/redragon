9ee5aa7003e11715e9785ce0c97d09cfdc0ba8a4
Abdellah <abdoessordo01@gmail.com>
2024-02-17 21:31:32 +0100

    new database

--- server/config/database.js
+++ server/config/database.js
@@ -1,23 +1,24 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const Connection = {
-  dialect: "postgres",
-  host: process.env.DB_HOST,
-  port: "5432",
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  logging: false,
-  protocol: "postgres",
-  dialectOptions: {
-    ssl: true,
-    native: true,
-  },
-};
+// const Connection = {
+//   dialect: "postgres",
+//   host: process.env.DB_HOST,
+//   port: "5432",
+//   username: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   logging: false,
+//   protocol: "postgres",
+//   dialectOptions: {
+//     ssl: true,
+//     native: true,
+//   },
+// };
 
 
-const sequelize = new Sequelize(Connection);
+// const sequelize = new Sequelize(Connection);
+const sequelize = new Sequelize(process.env.DB_URI)
 sequelize.sync({ alter: true });
 console.log("All models were synchronized successfully.");
 sequelize.query("CREATE EXTENSION IF NOT EXISTS pg_trgm", { raw: true });
